097ad3916d3f910e3cc0852ee1c981a4322741ea
ancrait <xdxddclash@gmail.com>
2026-06-22 09:54:26 +0300

    init fetch-service

---
+++ price-fetcher/src/main/java/com/sorokaandriy/price_fetcher/PriceFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.sorokaandriy.price_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceFetcherApplication.class, args);
+	}
+
+}
