23acfabd72333bf523c9b5ca8020951ca5193811
ancrait <xdxddclash@gmail.com>
2026-06-23 22:13:27 +0300

    create entity and repository

---
+++ price-alert/src/main/java/com/sorokaandriy/price_alert/dto/UserAlertNotification.java
@@ -0,0 +1,26 @@
+package com.sorokaandriy.price_alert.dto;
+
+
+import com.sorokaandriy.price_alert.entity.enumeration.Direction;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserAlertNotification {
+    private Long chatId;
+    private String symbol;
+    private BigDecimal updatedPrice;
+    private BigDecimal targetPrice;
+    private Direction direction;
+    private Boolean triggered;
+    private Instant updatedAt;
+
+}
